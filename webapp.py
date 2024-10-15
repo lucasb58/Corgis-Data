2a7f25b75386965b33b39e6c12a68236e50f2918
unknown <mrlucasblessing@gmail.com>
2024-10-15 13:57:56 -0700

    started graph

--- webapp.py
+++ webapp.py
@@ -6,11 +6,14 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 def render_main():
     return render_template('home.html')
     
-@app.route("/graph")
-def render_main_graph():
-    return render_template('graph.html')
+@app.route("/graphmath")
+def render_main_graph_math():
+    return render_template('graphmath.html')
     
-
+@app.route("/graphverbal")
+def render_main_graph_reading():
+    return render_template('graphverbal.html')
+    
     
 
 if __name__=="__main__":
