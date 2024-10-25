3317e0a56cc3805ac42d560f6a9da9c5312ef6e6
unknown <mrlucasblessing@gmail.com>
2024-10-25 13:22:59 -0700

    almost done

--- webapp.py
+++ webapp.py
@@ -13,35 +13,38 @@ def render_main():
     
 @app.route("/graphmath")
 def render_main_graph_math():
-    mathscore = get_math_scores()
-    return render_template('graphmath.html', graph_math_points = mathscore)
+    states = get_state_options()
+    state = request.args.get('state')
+    mathscore = get_math_scores(state)
+    return render_template('graphmath.html', graph_math_points = mathscore,  state_options=states)
 
 @app.route("/graphverbal")
 def render_main_graph_verbal():
     states = get_state_options()
     state = request.args.get('state')
-    verbalscore = get_verbal_scores()
-    return render_template('graphverbal.html', graph_verbal_points = verbalscore, state_options=state)    
+    verbalscore = get_verbal_scores(state)
+    place = "Verbal Scores in " + state + ", from 2005 to 2015."
+    return render_template('graphverbal.html', graph_verbal_points = verbalscore, state_options=states, title=place)    
     
 def get_state_options():
     with open('school_scores.json') as scores_data:
         state = json.load(scores_data)
     states=[]
-    for s in state:
-        if s["State"]["Code"] not in states:
-            states.append(s["State"])
+    for c in state:
+        if c["State"]["Code"] not in states:
+            states.append(c["State"]["Code"])
+    print(states)        
     options=""
     for s in states:
         options += Markup("<option value=\"" + s + "\">" + s + "</option>") #Use Markup so <, >, " are not escaped lt, gt, etc.
-    print(options)
     return options
 
-def get_math_scores():
+def get_math_scores(state):
     with open('school_scores.json') as scores_data:
         sat_scores = json.load(scores_data)
     mathscore={}
     for m in sat_scores:      
-        if m['State']['Code']== 'CA':
+        if m['State']['Code']== state:
             mathscore[str(m['Year'])] = m['Total']['Math']
     print(mathscore)
     graph_math_points= ""
@@ -49,12 +52,12 @@ def get_math_scores():
         graph_math_points= graph_math_points + Markup('{ x: ' + str(key) + ', y: ' + str(value) + ' },')        
     return graph_math_points 
     
-def get_verbal_scores():
+def get_verbal_scores(state):
     with open('school_scores.json') as scores_data:
         sat_scores = json.load(scores_data)
     verbalscore={}
     for v in sat_scores:      
-        if v['State']['Code']== 'CA':
+        if v['State']['Code']== state:
             verbalscore[v['Year']] = v['Total']['Verbal']
     print(verbalscore)  
     graph_verbal_points= ""
